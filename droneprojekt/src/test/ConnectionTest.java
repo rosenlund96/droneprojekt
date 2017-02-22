a9981ea3329e3d8c7dda2c3dd524b9cc366a2fc2
Mathias R. Larsen <mrl@cureit.dk>
2017-02-22 21:49:44 +0100

    updates

--- droneprojekt/src/test/ConnectionTest.java
+++ droneprojekt/src/test/ConnectionTest.java
@@ -1,5 +1,31 @@
 package test;
 
+import de.yadrone.base.ARDrone;
+import de.yadrone.base.IARDrone;
+import de.yadrone.base.command.LEDAnimation;
+
 public class ConnectionTest {
 
+	public static void main(String[] args)
+	{
+	    IARDrone drone = null;
+	    try
+	    {
+	        drone = new ARDrone();
+	        drone.start();
+	        drone.getCommandManager().setLedsAnimation(LEDAnimation.SNAKE_GREEN_RED, 10, 2);
+	        System.out.println("Der blev oprettet forbindelse til dronen, og LED animationen blev kørt");
+	    }
+	    catch (Exception exc)
+		{
+	    	System.out.println("Der skete en fejl:");
+			exc.printStackTrace();
+		}
+		finally
+		{
+			if (drone != null)
+				drone.stop();
+			System.exit(0);
+		}
+	}
 }
